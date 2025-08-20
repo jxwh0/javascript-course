2f2ac32996d6725d648dc66e6fb8fa2adb7d7ec1
jehu gipaya <jehusanluisgipaya@gmail.com>
2025-08-20 13:12:41 +0800

    feat: add basic variables and data types
    - Set up JavaScript environment and browser console
    - Implement let, const, var variable declarations
    - Add data type exploration with typeof operator
    - Establish development workflow and best practices

--- 01-fundamentals-part-1/starter/script.js
+++ 01-fundamentals-part-1/starter/script.js
@@ -1,2 +1,24 @@
 // JavaScript Fundamentals - Part 1
 // We'll write our code here!
+
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+console.log("GUMANA KA PLS");
+
+
+console.log("=== VARIABLES ===");
+
+let firstname = "Jehu";
+console.log(firstname);
+
+let age= 30;
+console.log(age);
+age = 50;
+console.log(age);
+
+const birthYear = 2005;
+console.log(birthYear);
+
+const PI = 3.1415;
+console.log(PI);
+
