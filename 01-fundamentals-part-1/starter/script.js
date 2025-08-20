5c8ae343f0874990d7cfeb9878ca407af8b4e6a2
jehu gipaya <jehusanluisgipaya@gmail.com>
2025-08-20 14:46:15 +0800

    nk u po

--- 01-fundamentals-part-1/starter/script.js
+++ 01-fundamentals-part-1/starter/script.js
@@ -1,24 +1,179 @@
-// JavaScript Fundamentals - Part 1
-// We'll write our code here!
+// // JavaScript Fundamentals - Part 1
+// // We'll write our code here!
 
-let js = "amazing";
-console.log(40 + 8 + 23 - 10);
-console.log("GUMANA KA PLS");
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
+// console.log("GUMANA KA PLS");
 
 
-console.log("=== VARIABLES ===");
+// console.log("=== VARIABLES ===");
 
-let firstname = "Jehu";
-console.log(firstname);
+// let firstname = "Jehu";
+// console.log(firstname);
 
-let age= 30;
-console.log(age);
-age = 50;
-console.log(age);
+// let age= 30;
+// console.log(age);
+// age = 50;
+// console.log(age);
 
-const birthYear = 2005;
-console.log(birthYear);
+// const birthYear = 2005;
+// console.log(birthYear);
 
-const PI = 3.1415;
-console.log(PI);
+// const PI = 3.1415;
+// console.log(PI);
 
+// var job ="programmer";
+// job = "teacher";
+// console.log(job);
+
+// let lastName = "Gipaya"
+
+// console.log("=== DATA TYPES ===");
+
+// age = 24;
+// console.log(age);
+// console.log(typeof age);
+
+// //string
+// let username ="jxwho";
+// console.log(username);
+// console.log(typeof username);
+
+// //Boolean
+// let javaScriptIsFun = true;
+// console.log(javaScriptIsFun);
+// console.log(typeof javaScriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// //dynamic variable
+// let dynamicVariabale = 23;
+// console.log(dynamicVariabale, typeof dynamicVariabale);
+
+// // Change into string
+// dynamicVariable = "NOw I'm a string";
+// console.log(dynamicVariabale, typeof dynamicVariabale);
+
+// // Change into a boolean
+// dynamicVariabale = true;
+// console.log(dynamicVariabale, typeof dynamicVariabale);
+
+//Basic Operators - Math Operators
+console.log("=== MATH OPERATORS ===");
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+console.log("Math operations:");
+console.log("Addition:", 10 + 5);
+console.log("Subtraction:", 20 - 8);
+console.log("Multiplication:", 4 * 7);
+console.log("Division:", 15 / 3);
+console.log("Exponentiation:", 2 ** 3);
+
+const firstName =  "Jonas";
+const lastName = "Doe";
+console.log(firstName + " " + lastName);
+console.log("Hello" + "World" + "!");
+console.log("I am " + 25 + " years old");
+
+//Assignment Operators
+console.log("=== ASSIGNEMNT OPERATORS ===");
+
+let x = 10 + 5;
+console.log("x starts as:", x);
+
+x +=10;
+console.log("After x += 10:", x);
+
+x *= 4; 
+console.log("After x *= 4:", x);
+
+x /= 2; 
+console.log("After x /= 2:", x);
+
+x++; 
+console.log("After x++:", x);
+
+x--; 
+x--; 
+console.log("After x-- twice:", x);
+
+/////
+console.log("=== COMPARISON OPERATORS ===");
+
+console.log("Age Comparison");
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+console.log(ageJonas < 30);
+
+console.log("Number comparisons:");
+console.log(25 > 20);
+console.log(15 < 10);
+console.log(18 >= 18);
+console.log(16 <= 15);
+
+////
+const isFullAge = ageSarah >= 18;
+console.log("Sarah is adult:", isFullAge);
+
+const isJonasOlder = ageJonas > ageSarah;
+console.log("Jonas is older:", isJonasOlder);
+
+console.log("Complex comparison");
+console.log(now - 1991 > now - 2018);
+
+let zx, y; 'Doe'
+z = y = 25 - 10 - 5;
+console.log(z, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
+
+////////////////////////////////////
+// Coding Challenge #1 - BMI Calculator
+
+// Test Data 1
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+// Your code here:
+// 1. Calculate BMIs
+const markBMI = (massMark / heightMark ** 2);
+const johnBMI = (massJohn / heightJohn ** 2);
+console.log(markBMI);
+console.log(johnBMI);
+// 2. Create markHigherBMI variable
+const markHigherBMI = markBMI > johnBMI;
+// 3. Log results to console
+console.log("Mark's BMI:", markBMI);
+console.log("John's BMI:", johnBMI);
+console.log("Mark has a higher BMI", markHigherBMI);
+
+////////////////////////////////////
+// Strings and Template Literals
+const firstNamee = "Jonas";
+const job = "teacher";
+const birthYear = 1991;
+const year = 2037;
+
+const jonas =
+  "I'm " + firstName + ", a " + (year - birthYear) + " year old " + job + "!";
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+console.log(jonasNew);
+
+console.log(`I'm ${2037 - 1991} years old`);
+console.log(`Math works: ${2 + 3} equals five`);
+console.log(`Comparisons too: ${5 > 3}`);
+
+console.log(`Just a regular string...`);
