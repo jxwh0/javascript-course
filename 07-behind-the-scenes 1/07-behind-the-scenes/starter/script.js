c0ee1e176b2a972056feb2dd33181f3cd0964492
Jehu Gipaya <jehusanluisgipaya@gmail.com>
2025-09-24 13:57:40 +0800

    const problemObject = {   name: 'Test',   date: new Date(),   method: function () {     return 'hello';   },   undefinedValue: undefined, };
    // BROKEN: JSON method loses data
    const brokenCopy = JSON.parse(JSON.stringify(problemObject));
    console.log('Broken copy:', brokenCopy);
    // { name: 'Test', date: '2023-...' } - Lost method and undefined!
    
    // FIXED: structuredClone preserves most types
    const workingCopy = structuredClone(problemObject);
    console.log('Working copy:', workingCopy);
    // Note: functions still can't be cloned, but dates work!

--- 07-behind-the-scenes 1/07-behind-the-scenes/starter/script.js
+++ 07-behind-the-scenes 1/07-behind-the-scenes/starter/script.js
@@ -1,75 +1,164 @@
-'use strict';
+// 'use strict';
 
-const person = {
-  name: 'Jonas',
-  greet: function () {
-    console.log(`Hello I am ${this.name}`);
-  },
+// const person = {
+//   name: 'Jonas',
+//   greet: function () {
+//     console.log(`Hello I am ${this.name}`);
+//   },
+// };
+
+// person.greet();
+
+// const anotherPerson = {
+//   name: 'Sarah',
+// };
+
+// anotherPerson.greet = person.greet;
+// anotherPerson.greet();
+
+// // detached function
+// const greetFunction = person.greet;
+// greetFunction();
+
+// const obj = {
+//   name: 'Object',
+
+//   regularMethod: function () {
+//     console.log('Regular:', this.name);
+//   },
+
+//   arrowMethod: () => {
+//     console.log('Arrow:', this.name);
+//   },
+// };
+
+// obj.regularMethod();
+// obj.arrowMethod();
+
+// const quiz = {
+//   name: 'Quiz Object',
+//   regularMethod() {
+//     console.log('Regular:', this.name);
+//   },
+//   arrowMethod: () => {
+//     console.log('Arrow:', this.name);
+//   },
+// };
+
+// quiz.regularMethod(); // Predict: ?
+// quiz.arrowMethod(); // Predict: ?
+
+// const timer = {
+//   name: 'Timer',
+
+//   // Old approach with self = this
+//   start: function () {
+//     console.log(`${this.name} starting...`);
+//     const self = this;
+
+//     setTimeout(function () {
+//       console.log(`${self.name} finished`);
+//     }, 1000);
+//   },
+
+//   // Modern approach with arrow function
+//   startModern: function () {
+//     console.log(`${this.name} starting modern...`);
+
+//     setTimeout(() => {
+//       console.log(`${this.name} finished modern`); // this works!
+//     }, 1500);
+//   },
+// };
+
+// timer.start(); // Uses self = this approach
+// timer.startModern(); // Uses arrow function approach
+
+// const user = {
+//   name: 'Alice',
+//   hobbies: ['reading', 'coding', 'gaming'],
+
+//   // BROKEN: arrow function loses `this`
+//   printHobbiesBad: () => {
+//     this.hobbies.forEach(hobby => {
+//       console.log(`${this.name} likes ${hobby}`); // this.name is undefined!
+//     });
+//   },
+
+//   // FIXED: regular function preserves `this`
+//   printHobbiesGood() {
+//     this.hobbies.forEach(hobby => {
+//       console.log(`${this.name} likes ${hobby}`); // this.name works!
+//     });
+//   },
+// };
+
+// user.printHobbiesBad(); // Fails - this.name is undefined
+// user.printHobbiesGood(); // Works - shows all hobbies
+
+// const functionTypes = {
+//   regularFunction: function () {
+//     console.log('Arguments length:', arguments.length);
+//     console.log('First argument:', arguments[0]);
+//   },
+
+//   arrowFunction: () => {
+//     console.log('Arrow function called');
+//   },
+
+//   modernFunction: (...args) => {
+//     console.log('Args length:', args.length);
+//     console.log('First arg:', args[0]);
+//   },
+// };
+
+// functionTypes.regularFunction('hello', 'world');
+// functionTypes.arrowFunction('test');
+// functionTypes.modernFunction('modern', 'approach');
+
+// PRIMITIVES IN STACK
+let age = 30;
+let oldAge = age;
+age = 31;
+
+console.log('age:', age);
+console.log('oldAge:', oldAge);
+
+const me = { name: 'Jonas', age: 30 };
+const friend = me;
+
+friend.age = 27;
+
+console.log('me:', me);
+console.log('friend:', friend);
+
+function changeAge(person, newAge) {
+  person.age = newAge;
+  return person;
+}
+
+const originalPerson = { name: 'Sarah', age: 25 };
+const updatedPerson = changeAge(originalPerson, 30);
+
+console.log('original:', originalPerson);
+console.log('updated:', updatedPerson);
+console.log('same object?:', originalPerson === updatedPerson);
+
+const original = {
+  name: 'Alice',
+  age: 28,
+  hobbies: ['reading', 'coding'],
 };
 
-person.greet();
+const shallowCopy = { ...original };
 
-const anotherPerson = {
-  name: 'Sarah',
-};
+shallowCopy.name = 'Bob';
+console.log('original name:', original.name);
+console.log('copy name:', shallowCopy.name);
 
-anotherPerson.greet = person.greet;
-anotherPerson.greet();
+shallowCopy.hobbies.push('gaming');
+console.log('original hobbies:', original.hobbies);
+console.log('copy hobbies:', shallowCopy.hobbies);
 
-// detached function
-const greetFunction = person.greet;
-greetFunction();
-
-const obj = {
-  name: 'Object',
-
-  regularMethod: function () {
-    console.log('Regular:', this.name);
-  },
-
-  arrowMethod: () => {
-    console.log('Arrow:', this.name);
-  },
-};
-
-obj.regularMethod();
-obj.arrowMethod();
-
-const quiz = {
-  name: 'Quiz Object',
-  regularMethod() {
-    console.log('Regular:', this.name);
-  },
-  arrowMethod: () => {
-    console.log('Arrow:', this.name);
-  },
-};
-
-quiz.regularMethod(); // Predict: ?
-quiz.arrowMethod(); // Predict: ?
-
-const timer = {
-  name: 'Timer',
-
-  // Old approach with self = this
-  start: function () {
-    console.log(`${this.name} starting...`);
-    const self = this;
-
-    setTimeout(function () {
-      console.log(`${self.name} finished`);
-    }, 1000);
-  },
-
-  // Modern approach with arrow function
-  startModern: function () {
-    console.log(`${this.name} starting modern...`);
-
-    setTimeout(() => {
-      console.log(`${this.name} finished modern`); // this works!
-    }, 1500);
-  },
-};
-
-timer.start(); // Uses self = this approach
-timer.startModern(); // Uses arrow function approach
+const anotherCopy = Object.assign({}, original);
+console.log('Object.assign copy:', anotherCopy);
